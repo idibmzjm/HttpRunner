96b97d8179355028ce1332e489e0285eacf21891
张建明 <jimmy.zhangjm@dadaabc.com>
2019-05-12 00:56:39 +0800

    add

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(here, 'apiautotest', '__about__.py'), encoding='utf-8') as f:
     exec(f.read(), about)
 
-with io.open("README.md", encoding='utf-8') as f:
-    long_description = f.read()
-
 install_requires = [
     "requests",
     "PyYAML",
@@ -25,58 +22,17 @@ install_requires = [
     "filetype"
 ]
 
-class UploadCommand(Command):
-    """ Build and publish this package.
-        Support setup.py upload. Copied from requests_html.
-    """
-
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in green color."""
-        print("\033[0;32m{0}\033[0m".format(s))
-
-    def initialize_options(self):
-        """ override
-        """
-        pass
-
-    def finalize_options(self):
-        """ override
-        """
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Publishing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
 
 setup(
     name=about['__title__'],
     version=about['__version__'],
     description=about['__description__'],
-    long_description=long_description,
+    long_description=about['__description__'],
     long_description_content_type='text/markdown',
     author=about['__author__'],
     author_email=about['__author_email__'],
     url=about['__url__'],
-    license=about['__license__'],
+    license="GPL",
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
     packages=find_packages(exclude=["examples", "tests", "tests.*"]),
     package_data={
@@ -101,9 +57,5 @@ setup(
             'hrun=apiautotest.cli:main_hrun',
             'locusts=apiautotest.cli:main_locust'
         ]
-    },
-    # $ setup.py upload support.
-    cmdclass={
-        'upload': UploadCommand
     }
 )
